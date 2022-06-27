aefdd0ab69dfda3d6fdaca526c2f383c36bcdcdb
Adam <adam.jkirjacev@gmail.com>
2022-06-27 15:17:21 -0700

    comment

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const input = document.getElementById("input");
 const taskList = document.getElementById("task-container");
 let tasks = {};
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
+form.addEventListener("submit", function(event){
+    event.preventDefault();
     console.log("button clicked");
   });
 
-
+/*
 
 let newTask = () => {
     newTask["text"] = input.value;
     console.log(newTask);
 };
+*/
