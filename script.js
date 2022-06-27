e35b38485b214b78d429f2d253c83ef307d7c278
Adam <adam.jkirjacev@gmail.com>
2022-06-27 14:49:19 -0700

    1

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const taskList = document.getElementById("task-container");
 let tasks = {};
-form.addEventListener("submit", (e) => {
+
+form.addEventListener("submit", function(e){
     e.preventDefault();
     console.log("button clicked");
   });
