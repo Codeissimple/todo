4a7195e3f8b7187e36f65c7547aacd5173159a82
Adam <adam.jkirjacev@gmail.com>
2022-06-27 20:20:58 -0700

    still not working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("form");
+var form = document.getElementById("form");
 const inputValue = document.getElementById("input");
 const taskList = document.getElementById("taskList");
 
@@ -6,14 +6,15 @@ let tasks = [];
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    addTask(input.value);  //Here I take the user input.
+    var newInput = input.value;
+    addTask(newInput.value);  //Here I take the user input.
     console.log(input.value);
     console.log("button clicked");
     form.reset();
   });
 //Here I am pasting the user input into the addTask() function, but 
 //it reads "null"(?)
-function addTask(e){
+function addTask(newtask){
   const taskInstance = document.createElement("li");
   const checkButton = document.createElement("div");
   const taskText = document.createElement("span");
@@ -22,7 +23,7 @@ function addTask(e){
   checkButton.setAttribute("class", "");
   taskText.setAttribute("class", "");
 
-  taskText.innerText = e;
+  taskText.innerText = newtask;
   taskList.appendChild(taskInstance);
   taskInstance.appendChild(checkButton);
   taskInstance.appendChild(taskText);
