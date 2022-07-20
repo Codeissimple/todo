dd0937b8eb862636dcc06f19723c07664cb00fef
Adam <adam.jkirjacev@gmail.com>
2022-07-20 09:10:33 -0700

    V5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var form = document.getElementById("form");
-const inputValue = document.getElementById("input");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
 const taskList = document.getElementById("taskList");
 const taskObject = document.querySelector("task-instance-class");
 
@@ -7,15 +7,46 @@ let tasks = [];
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    var newInput = input.value;
-    addTask(newInput); 
+    let newInput = input.value;
+    tasks.push(createTask(newInput))
     form.reset();
+    //console.log(tasks);
+    renderList();
   });
 
 
-function addTask(newTask){
-
+function createTask(newTask){
+  return{
+    taskName: newTask,
+    completed: false,
   }
+  }
+
+  function renderList(){
+    taskList.innerHTML = "";
+    tasks.forEach(function(element, index)  {
+      let newEntry = `<li> <input type='checkbox'> ${element.taskName}<button></button></li>`;
+      taskList.innerHTML += newEntry;
+    });
+    
+  }
+
+  taskList.addEventListener('click', function(e){
+    let item = e.target.parentElement.innerText;
+    console.log("item: " + item);
+    
+    tasks.forEach(element => {
+
+      if(element.taskName === item){
+        element.completed = !element.completed;
+        console.log('taskName: ' + element.taskName);
+      }
+
+    });
+    //console.log(tasks);
+  })
+
+
 
   
   
