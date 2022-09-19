1316d6315b66bce30deeb50a3328f2e5f2270692
Codeissimple <adam.jkirjacev@gmail.com>
2022-09-19 15:30:27 -0400

    122

--- script.js
+++ script.js
@@ -1,56 +1,104 @@
-//minimum of what I need to grab from DOM
 const myForm = document.querySelector('form');
-const myTaskList = document.querySelector('taskList');
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-const localStorageTasks = JSON.parse(localStorage.getItem(''))
+const myTaskList = document.getElementById('taskList');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 //separate eventListeners from the functions to simplify code
 myForm.addEventListener('submit', addTask);
 
 
 //Calling function that's going to display tasks
-renderTasks();
+window.onload = renderTasks();
 
 function addTask(element) {
   element.preventDefault();
   const taskText = this.querySelector('[name=input]').value;
   const task = {
     taskText, 
-    done:false, 
+    done: false, 
     ID: Date.now()
   }
-  console.log(taskText);
 
+//creating li element
+  let newEntry = document.createElement('li');
+    newEntry.innerHTML = task.taskText;
+    newEntry.done = false;
+    newEntry.ID = task.ID;
+
+//creating checkbox element
+  let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+//creating delete button
+  let deleteButton = document.createElement('button')
+    deleteButton = 'X';
+
+
+// putting task visuals together
+  newEntry.prepend(checkbox);
+  newEntry.append(deleteButton);
+
+  myTaskList.appendChild(newEntry);
   tasks.push(task);
   push2Local();
-  renderTasks();
-
-  this.reset();
-
+  console.log(tasks);
+  this.reset();   
 }
 
 function push2Local() {
-  localStorage.setItem('tasks',JSON.stringify(localStorageTasks));
+  console.log(tasks);
+  localStorage.setItem('tasks',JSON.stringify(tasks));
+  console.log('pushed task to local');
 }
 
+
 function renderTasks() {
-  let taskArray = localStorage.getItem('tasks',JSON.stringify(localStorageTasks));
+
+console.log('RENDER');
+let taskArray = localStorage.getItem('tasks',JSON.stringify(tasks));
+
+console.log(localStorage.getItem('tasks'));
+
+  if(localStorage.getItem('tasks') === null) {
+      tasks = [];
+  } else {
+      taskArray = JSON.parse(localStorage.getItem('tasks'));
   
-  let taskDisplayUL = document.getElementById('taskList');
+      taskArray.forEach(task => {
+          console.log(task.taskText)
+          let newEntry = document.createElement('li');
+          newEntry.innerHTML = task.taskText;
+          newEntry.done = false;
+          newEntry.ID = task.ID;
+      
+      
+      //creating checkbox element
+        let checkbox = document.createElement('input');
+          checkbox.type = 'checkbox';
+      
+      //creating delete button
+        let deleteButton = document.createElement('button')
+          deleteButton = 'X';
+          deleteButton.classList.add('delButton');
+      
+      
+      // putting task visuals together
+        newEntry.prepend(checkbox);
+        newEntry.append(deleteButton);
+      //-----------------------
+        //let newEntry = `<li><input type='checkbox'>${task.taskText}<button class='buttonDelete'>X</button></li>`;
+        //console.log(newEntry); 
+      //-----------------------
+        myTaskList.appendChild(newEntry);
+      });
+}
+};
 
+const li = document.getElementById('li');
+console.log(li);
 
-  tasks.forEach(display);
-  function display(element) {
-    let newEntry = `<li><input type='checkbox' ${element.completed ? 'checked' : null}>${element.taskName}<button class='buttonDelete'>X</button></li>`;
-  console.log(newEntry); 
-  taskDisplayUL.append(newEntry);     
-    }
-    
-
-
-
-  };
+myTaskList.addEventListener('click', function deleteTask(){
+  console.log(this);
+}); 
 
 
   
