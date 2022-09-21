a6f33c4042ea7d4650968b8801cd353f4c7b3b81
Codeissimple <adam.jkirjacev@gmail.com>
2022-09-21 18:30:58 -0400

    final

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 //separate eventListeners from the functions to simplify code
 myForm.addEventListener('submit', addTask);
 
-
 //Calling function that's going to display tasks
 window.onload = renderTasks();
 
@@ -24,15 +23,9 @@ function addTask(element) {
     newEntry.done = false;
     newEntry.setAttribute('id',task.id);
 
-//creating checkbox element
-  let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-
 //creating delete button
   let deleteButton = document.createElement('button')
-    //deleteButton.innerText = 'X';
     deleteButton.classList.add('delButton');
-
 
 // putting task visuals together
   newEntry.prepend(checkbox);
@@ -51,13 +44,9 @@ function push2Local() {
   console.log('pushed task to local');
 }
 
-
 function renderTasks() {
-
 console.log('RENDER');
 let taskArray = localStorage.getItem('tasks',JSON.stringify(tasks));
-
-//console.log(localStorage.getItem('tasks'));
 
   if(localStorage.getItem('tasks') === null) {
       tasks = [];
@@ -65,62 +54,41 @@ let taskArray = localStorage.getItem('tasks',JSON.stringify(tasks));
       taskArray = JSON.parse(localStorage.getItem('tasks'));
   
       taskArray.forEach(task => {
-          //console.log(task.taskText)
+
           let newEntry = document.createElement('li');
           newEntry.innerHTML = task.taskText;
           newEntry.done = false;
           newEntry.setAttribute('id',task.id);
-      
-      
-      //creating checkbox element
-        let checkbox = document.createElement('input');
-          checkbox.type = 'checkbox';
           
-      
       //creating delete button
       let deleteButton = document.createElement('button')
-      //deleteButton.innerText = 'X';
       deleteButton.classList.add('delButton');
       
-      
       // putting task visuals together
-        newEntry.prepend(checkbox);
         newEntry.append(deleteButton);
-      //-----------------------
-        //let newEntry = `<li><input type='checkbox'>${task.taskText}<button class='buttonDelete'>X</button></li>`;
-        //console.log(newEntry); 
-      //-----------------------
+
         myTaskList.appendChild(newEntry);
       });
 }
 };
 
 const delButton = document.getElementsByClassName('delButton');
-//console.log(delButton);
 
-
-
-myTaskList.addEventListener("click", function(event) {
-
-    let IDinterest = parseInt(event.target.parentNode.id, 10);
-      console.log(IDinterest);
-      console.log('tasks reachable');
-    for(i = 0; i < tasks.length; i++){
-      console.log(tasks[i]);
-      if(IDinterest === tasks[i].id){
-        //tasks.remove(tasks[i]);
-        console.log('aye, aye ', tasks[i]);
-      }
-    }
-
-    const targetTagToLowerCase = event.target.tagName.toLowerCase();
+myTaskList.addEventListener("click", function (event) {
+  const targetTagToLowerCase = event.target.tagName.toLowerCase();
   if (targetTagToLowerCase === "li") {
-    event.target.style.textDecoration = "line-through";
-    console.log(target);
+      event.target.style.textDecoration = "line-through";
   } else if (targetTagToLowerCase === "button") {
-    event.target.parentNode.remove();
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+      event.target.parentNode.remove();
+      let IDinterest = Number(event.target.parentNode.id);
+      console.log(IDinterest);
+      for (let i = 0; i < tasks.length; i++) {
+          if (IDinterest === tasks[i].id) {
+              tasks.splice(i, 1);
+              console.log('aye')
+          }
+      }
+      localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 });
 
